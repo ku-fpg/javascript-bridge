4cb60de1828b164366aa79d1b9fcb58b28bc5aff
Andy Gill <andygill@ku.edu>
2018-12-23 10:45:49 -0800

    Fix benchmarks to have 2 params

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+-- stack exec javascript-bridge-benchmarks -- -o benchmarks.html
 import Criterion.Main
 
 import Control.Applicative
@@ -12,17 +13,22 @@ import qualified Data.Text.Lazy as T
 import Network.JavaScript
 import System.Exit
 import Web.Scotty
+import Control.Monad(foldM)
 
+-- The function we're benchmarking.
+fib :: Int -> Int
+fib m | m < 0     = error "negative!"
+      | otherwise = go m
+  where
+    go 0 = 0
+    go 1 = 1
+    go n = go (n-1) + go (n-2)
 
 -- Our benchmark harness.
 main = do
-        lock <- newEmptyMVar
-
-        _ <- forkIO $ scotty 3000 $ do
+        scotty 3000 $ do
           middleware $ start $ \ e -> test e `E.finally`
-                       (do putMVar lock ()
-                           putStrLn "Finished example")
-
+                       (putStrLn "Finished example")
 
           get "/" $ do
             html $ mconcat
@@ -36,14 +42,25 @@ main = do
                ,   "</script>"
                , "</body>"
                ]
+  
+          get "/benchmarks.html" $
+            file "benchmarks.html"
 
-        takeMVar lock
 
-call :: Engine -> Int -> IO (Result Int)
-call e n = fromJSON <$> send e (sequenceA [ command $ val n | i <- [1..n]] *> (procedure $ val n))
+call :: (Applicative f, Command f, Procedure f) => Int -> f Int
+call n = sequenceA [ command $ value n | i <- [1..n]] *> (procedure $ value n)
 
-test e = 
-  defaultMain [ bench (show n) $ whnfIO (call e n)
-              | n <- [0,10,100,1000,10000]
+chain :: Engine -> Int -> Int -> IO Int
+chain e n m = send e $ foldM (\ r i -> call m) 0 [1..n]
+
+test e = do
+  send e $ command $ "document.getElementById('cursor').innerHTML +='Starting tests... '"
+  defaultMain [
+    bgroup ("chain of " ++ show n ++ " element(s)") [
+        bench (show m ++ " command(s) in packet") $ whnfIO (chain e n m)
+              | m <- [0,10,100,1000]
               ]
+         | n <- [1,10,100]
+         ]
+  send e $ command $ "document.getElementById('cursor').innerHTML +='<a href=\"benchmarks.html\">finished</a>'"
   
