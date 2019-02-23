2f63c57960c2a6b08d768e0f0916a574f9615db6
Andy Gill <andygill@ku.edu>
2019-02-23 12:31:09 -0800

    Adding NoFuture constructor
    The only strange place is inside bind, which can return a future that contains a retry.

--- src/Network/JavaScript/Future.hs
+++ src/Network/JavaScript/Future.hs
@@ -8,12 +8,15 @@ import Control.Concurrent.STM
 import Data.Semigroup
 import System.IO.Unsafe
 
-newtype Future a = Future (STM (TimeStamp,a))
+data Future a
+  = Future (STM (TimeStamp,a))
+  | NoFuture
 -- We could optimize Future, providing a
 --  * pure constructor (always has happened)
 --  * mempty constructor (never happens)
 
 {-# NOINLINE timeSupply #-}
+-- We intentually increment the epoch for the first stored value.
 timeSupply :: TVar TimeStamp
 timeSupply = unsafePerformIO $ newTVarIO $ succ epoch 
 
@@ -25,9 +28,12 @@ newtype TimeStamp = TimeStamp Int
  
 instance Functor Future where
   fmap f (Future m) = Future $ fmap (\ (t,a) -> (t,f a)) m
+  fmap _ NoFuture = NoFuture
 
 instance Applicative Future where
   pure a = Future $ pure (epoch,a)
+  NoFuture <*> _ = NoFuture
+  _ <*> NoFuture = NoFuture
   Future m1 <*> Future m2 = Future $ do
     (t1,r1) <- m1
     (t2,r2) <- m2
@@ -35,17 +41,21 @@ instance Applicative Future where
 
 instance Monad Future where
   return = pure
+  NoFuture >>= _ = NoFuture
   Future m1 >>= k = Future $ do
     (t1,r1) <- m1
     case k r1 of
       Future m2 -> do
         (t2,r2) <- m2
         return (t1 `max` t2,r2)
-
+      NoFuture -> retry
+        
 instance Semigroup (Future a) where
   -- Note: is it not possible to get a retry
   -- for r1 or r2, then later resolve to an *earlier* time,
   -- because of the global timeStamp namesupply.
+  NoFuture <> f = f
+  f <> NoFuture = f
   Future m1 <> Future m2 = Future $
      (do r1@(t1,_) <- m1
          ((\ r2@(t2,_) -> if t1 <= t2 then r1 else r2) <$> m2)
@@ -53,13 +63,14 @@ instance Semigroup (Future a) where
      ) `orElse` m2
 
 instance Monoid (Future a) where
-  mempty = Future $ retry
+  mempty = NoFuture
   mappend = (<>)
 
 force :: Future a -> IO (TimeStamp,a)
 force = atomically . forceSTM
 
 forceSTM :: Future a -> STM (TimeStamp,a)
+forceSTM NoFuture = retry
 forceSTM (Future m) = do
   (t,r) <- m
   return (t,r)
