8284028cbdd53f998b154c47019a218ba64f0653
Andy Gill <andygill@ku.edu>
2019-02-28 22:19:11 -0800

    Adding Slider example

---
+++ examples/Slider.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Data.Aeson             
+import Web.Scotty
+
+import Network.JavaScript.ElmArchitecture
+
+import Paths_javascript_bridge
+
+-- The Slider example of the elm architecture
+
+data Msg = SlideTo Double
+  deriving (Eq, Ord, Show)
+
+example :: ElmArchitecture () Msg Double
+example = ElmArchitecture{..}
+  where
+    update (SlideTo v) _ = pure v
+    view   n    = object <$> sequenceA
+        [ ("text" .=)   <$> pure  n
+        , ("slider" .=) <$> slider SlideTo
+        ]
+    runtime _ = error "no runtime needed"
+
+main :: IO ()
+main = main_ 3000
+
+main_ :: Int -> IO ()
+main_ i = do
+  dataDir <- getDataDir
+--  dataDir <- return "."
+  scotty i $ do
+    get "/" $ file $ dataDir ++ "/examples/Slider.html"
+    middleware $ elmArchitecture example 20
+            
