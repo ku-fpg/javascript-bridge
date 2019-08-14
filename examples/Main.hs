1e473a59889e1d56f39978da8667de18aa99f9c9
Andy Gill <andygill@ku.edu>
2019-08-14 13:30:31 -0500

    Add an example of vanilla JavaScript bridging

---
+++ examples/Main.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
+
+
+module Main where
+
+import Web.Scotty
+import Network.JavaScript
+  
+import Paths_javascript_bridge
+
+main :: IO ()
+main = main_ 3000
+
+main_ :: Int -> IO ()
+main_ i = do
+  dataDir <- getDataDir
+  -- dataDir <- return "." -- use for debugging
+  scotty i $ do
+    middleware $ start app  
+    get "/" $ file $ dataDir ++ "/examples/Main.html"
+
+
+app :: EventChan -> Engine -> IO ()
+app ev eng = do
+  send eng $ do
+    command $ call "console.log" [string "starting..."]
+    render "Hello!"
+
+-- It is good practice to reflect the JavaScript utilties
+-- you are using as typed Haskell functions.
+render :: Command f => String -> f ()
+render t = command $ call "jsb.render" [value t]
+    
+ 
